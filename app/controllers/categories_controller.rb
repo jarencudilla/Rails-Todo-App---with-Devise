6d5ef486db4c06b507733fa8f8410c4a2f7fed14
Jaren Cudilla <jarencudilla@gmail.com>
2021-07-11 19:10:59 +0800

    Added code to categories and tasks controllers, finished CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,55 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @categories = current_user.categories.order("created_at DESC")
   end
 
   def new
+    @category = current_user.categories.build
   end
 
-  def edit
+  def create
+    @category = current_user.categories.build(category_params)
+
+    if @category.valid?
+        @category.save
+        redirect_to home_path
+        flash[:notice] = "Category created!"
+    else
+      redirect_to new_category_path, alert: @category.errors.full_messages.first
+    end
   end
 
   def show
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def edit
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def update
+    @category = current_user.categories.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to home_path, notice: "Category successfully updated."
+
+    else
+      redirect_to edit_category_path, alert: @category.errors.full_messages.first
+    end
   end
 
   def destroy
+    @category = current_user.categories.find(params[:id])
+    @category.destroy
+    redirect_to home_path, notice: "Category successfully deleted."
   end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:name, :details)
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,61 @@
 class TasksController < ApplicationController
+before_action :authenticate_user!
+before_action :get_category
+
   def index
+    @tasks = @category.tasks
   end
 
   def new
+    @task = @category.tasks.build
   end
 
-  def edit
+  def create
+    @task = @category.tasks.build(task_params)
+
+      if @task.save
+      redirect_to category_path(@category)
+      flash[:notice] = 'Task created!'
+
+    else
+      redirect_to new_category_task_path, alert: @task.errors.full_messages.first
+    end
   end
 
   def show
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def edit
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def update
+    @task = @category.tasks.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to category_url(@category)
+      flash[:notice] = 'Task successfully updated.'
+
+    else
+      redirect_to category_task_path, alert: @task.errors.full_messages.first
+    end
   end
 
   def destroy
+    @task = @category.tasks.find(params[:id])
+    @task.destroy
+    redirect_to category_url(@category), notice: 'Task successfully deleted.'
   end
+
+  private
+
+  def get_category
+    @category = Category.find(params[:category_id]) #category id is the foreign key in the task
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :category_id)
+  end
+
 end
